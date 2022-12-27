0f96e8e86869580a2fb07a57025ba5cc7adf93ee
sudhavamsikiran damojipurapu <46421514+sudhavamsikiran@users.noreply.github.com>
2022-12-27 20:53:31 +0530

    JSworkshop files

---
+++ Javascriptworkspace/Chapter2/Exercise02/getelementbyidexample.js
@@ -0,0 +1,4 @@
+var jsframeworksList=document.getElementById('jsTypes');
+var newFramework = document.createElement('li');
+newFramework.innerText = "Vue JS";
+jsframeworksList.appendChild(newFramework);
---
+++ Javascriptworkspace/Chapter2/Exercise02/index.js
@@ -0,0 +1,5 @@
+var fname='sudha';
+var sname='kiran';
+var fullname=fname+'vamsi'+sname+''+' damojipurapu';
+console.log('fullname is ' +fullname);
+alert(fullname);
